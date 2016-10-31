ee0c155137ccbd1eb0afb31d2aeaf568667d7397
willimach <willi@mach.at>
2016-10-31 10:42:21 +0100

    loads of changes

--- weather.py
+++ weather.py
@@ -1,13 +1,18 @@
 from Adafruit_BME280 import *
 
-import  time, threading
+import  time, threading, os
 from datetime import datetime
 from weather_bib import *
 
 sensor1 = BME280(mode=BME280_OSAMPLE_8,address=0x76)
 sensor2 = BME280(mode=BME280_OSAMPLE_8,address=0x77)
-offset1=-2.0
-offset2=-1.5
+T_offset1=-2.0
+T_offset2=-1.8
+H_offset1=0.0
+H_offset2=0.0
+
+path='/home/pi/sharepi3/weather/'
+os.chdir(path)
 
 def logging():
     while True:
@@ -16,26 +21,33 @@ def logging():
             #if datetime.now().second % 5:
                 f=open('weatherlog.txt','a')
                 my_time = mytime()
-                mystring = str(my_time) + ', ' + str(scan(sensor1,offset1))[1:-1] + ', ' + str(scan(sensor2,offset2))[1:-1]
+                mystring = str(my_time) + ', ' + str(scan(sensor1,T_offset1,H_offset1))[1:-1] + ', ' + str(scan(sensor2,T_offset2,H_offset2))[1:-1]
                 f.write(mystring+'\n')
                 f.close()
                 #print('logged!')
-                time.sleep(60)
+            time.sleep(1)
         except:
+            #print('Log-Fehler')
             pass
+        time.sleep(60)
 def maintenance():
     while True:
         try:
             if (datetime.now().minute % 15) == 0:
             #if datetime.now().second % 5:
-                myplotter(1,4,'T')
-                myplotter(2,5,'H')
-                myplotter(3,6,'P')
+                myplotter(1,4,'T',0)
+                myplotter(2,5,'H',0)
+                myplotter(3,6,'P',0)
+                myplotter(1,4,'T',7)
+                myplotter(2,5,'H',7)
+                myplotter(3,6,'P',7)
                 myftp()
                 #print('maintenanced!')
-                time.sleep(60)
+            time.sleep(1)
         except:
+            #print('maint.Fehler')
             pass
+        time.sleep(60)
 # lock to serialize console output
 lock = threading.Lock()
     
@@ -47,4 +59,4 @@ t.daemon = True
 s.start()
 t.start()   
 while True:
-    time.sleep(1)
+    time.sleep(100)
--- weather_bib.py
+++ weather_bib.py
@@ -1,9 +1,9 @@
-
-def scan(sensor,offset):
-    temp = round(sensor.read_temperature()+offset,1)
-    humi = round(sensor.read_humidity(),1)
+def scan(sensor,T_offset,H_offset):
+    temp = round(sensor.read_temperature()+T_offset,1)
+    humi = round(sensor.read_humidity()+H_offset,1)
     press_sea = round(sensor.read_pressure()/100 + 12*3,1)
     return temp,humi,press_sea
+
 
 
 def mytime():
@@ -11,6 +11,8 @@ def mytime():
     mytime=datetime.datetime.now()
     date = "%04d.%02d.%02d_%02d:%02d:%02d" % (mytime.year, mytime.month, mytime.day, mytime.hour, mytime.minute, mytime.second)
     return(date)
+
+
 
 def plotextractor(a):
     f=open('weatherlog.txt','r')
@@ -24,31 +26,48 @@ def plotextractor(a):
         mylist.append(element[a])    
     return mylist
 
-def myplotter(column1,column2,quantity):
+
+
+def myplotter(column1,column2,quantity,plottimedays):
     # Force matplotlib to not use any Xwindows backend.
     import matplotlib, numpy, datetime
     matplotlib.use('Agg')
     import matplotlib.pyplot as plt
     from matplotlib.dates import DayLocator, HourLocator, DateFormatter, drange,date2num,num2date
     import gc
-    
+
+    # define time and y-lists for plotting
     timelist = plotextractor(0)
     y1 = plotextractor(column1)
     y2 = plotextractor(column2)
 
-    fig, ax = plt.subplots(figsize=(24,8))
+    #define image size
+    fig, ax = plt.subplots(figsize=(36,8))
+    #rotate dates on x-axis
     plt.xticks(rotation=45)
-    
-    plt.title('FGG36 weather' + '  '*40 + mytime(),loc='right')
+    #plot title
+    plt.title('FGG36 weather' + '  '*90 + mytime(),loc='right')
     
 
-
+    #change readable timestampt to datetime-objects
     x=[]
     for element in timelist:
         x.append(datetime.datetime(int(element[0:4]),int(element[5:7]),int(element[8:10]),int(element[11:13]),int(element[14:16])))
-    
-    date1 = datetime.datetime(int(timelist[0][0:4]),int(timelist[0][5:7]),int(timelist[0][8:10]))
-    date2 = datetime.datetime(int(timelist[-1][0:4]),int(timelist[-1][5:7]),int(timelist[-1][8:10])+1)
+
+    #distinguish between long and short plots - define plotstart
+    #(long means all date (0) and short means e.g. 7 dayss)
+    if plottimedays==0:
+        namestring='weather_'+quantity+'_all'
+        date1 = datetime.datetime(int(timelist[0][0:4]),int(timelist[0][5:7]),int(timelist[0][8:10]))
+    else:
+        namestring='weather_'+quantity+'_lastdays'
+        #subtract plottiedays from latest timestamp
+        date1 = datetime.datetime(int(timelist[-1][0:4]),int(timelist[-1][5:7]),int(timelist[-1][8:10])) + datetime.timedelta(days=1) + datetime.timedelta(days=-plottimedays)
+
+    #subtract 1 day from latest timestamp so that current day is full visible in plot        
+    date2 = datetime.datetime(int(timelist[-1][0:4]),int(timelist[-1][5:7]),int(timelist[-1][8:10])) + datetime.timedelta(days=1)
+
+
 
     ax.set_xlim(date1,date2)    
     ax.xaxis.set_major_locator(DayLocator())
@@ -59,14 +78,16 @@ def myplotter(column1,column2,quantity):
 
     ax.grid(True,which='minor',linestyle='--')
     ax.grid(True,which='major',linestyle='-')
-    
+
+    #define legend    
     plt.plot(x,y1,label="inside")
     plt.plot(x,y2,label="outside")
-    plt.legend(bbox_to_anchor=(0.95, 0.95), borderaxespad=0.)
+    plt.legend(bbox_to_anchor=(0.51, 0.95), borderaxespad=0.)
 
+    #save
+    plt.savefig(namestring + '.png', bbox_inches='tight')
 
-    plt.savefig(quantity + '.png',bbox_inches='tight')
-    
+    #clean to prevent full RAM
     plt.clf()
     plt.close()
     gc.collect()
@@ -84,19 +105,32 @@ def myftp():
     ftp=FTP(server,user=user,passwd=passwd)
     ftp.cwd('www')
 
+    #all time plots
+    file=open('weather_T_all.png','rb')
+    ftp.storbinary('STOR weather_T_all.png',file)
+    file.close()
 
-    file=open('H.png','rb')
-    ftp.storbinary('STOR H.png',file)
+    file=open('weather_P_all.png','rb')
+    ftp.storbinary('STOR weather_P_all.png',file)
     file.close()
-    
-    file=open('P.png','rb')
-    ftp.storbinary('STOR P.png',file)
+
+    file=open('weather_H_all.png','rb')
+    ftp.storbinary('STOR weather_H_all.png',file)
     file.close()
-    
-    file=open('T.png','rb')
-    ftp.storbinary('STOR T.png',file)
+    #7day plots
+    file=open('weather_T_lastdays.png','rb')
+    ftp.storbinary('STOR weather_T_lastdays.png',file)
     file.close()
-    
+
+    file=open('weather_P_lastdays.png','rb')
+    ftp.storbinary('STOR weather_P_lastdays.png',file)
+    file.close()
+
+    file=open('weather_H_lastdays.png','rb')
+    ftp.storbinary('STOR weather_H_lastdays.png',file)
+    file.close()   
+
+
     ftp.quit()
 
 
